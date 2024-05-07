4374553a586eeb51292a37d0d7c32abc8098750d
yusufakbs <akbasyusuuf@gmail.com>
2024-05-08 00:00:00 +0300

    Initial commit

---
+++ src/main/java/com/yusuf/BilgeAdam_Car_Dealership/BilgeAdamCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.yusuf.BilgeAdam_Car_Dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilgeAdamCarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilgeAdamCarDealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusuf/BilgeAdam_Car_Dealership/BilgeAdamCarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.BilgeAdam_Car_Dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilgeAdamCarDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
